400923cf748a0f00cb7bf74a12d07f7ef9ca2749
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-09-05 12:59:14 +0530

    starter file of a eat-n-split project

---
+++ 02-eat-n-split/src/App.jsx
@@ -0,0 +1,12 @@
+import FriendList from "./components/FriendList"
+
+function App() {
+  return (
+   <div className="app">
+   <FriendList />
+   </div>
+  )
+}
+
+export default App
+
---
+++ 02-eat-n-split/src/components/FriendList.jsx
@@ -0,0 +1,6 @@
+
+export default function FriendList() {
+  return (
+    <ul>FriendList</ul>
+  )
+}
