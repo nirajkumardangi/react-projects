5e4621e315d32f5d8c7984f8fdfc5d37333d1b90
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-09-06 12:02:17 +0530

    starter file

---
+++ 03-tip-calculator/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>Hello from React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 03-tip-calculator/src/main.jsx
@@ -0,0 +1,10 @@
+import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
