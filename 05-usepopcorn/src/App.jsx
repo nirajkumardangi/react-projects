23df616aa996db9e6c63e49de63de498f64ac9bb
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-09-18 16:23:38 +0530

    splitting components

--- 05-usepopcorn/src/App.jsx
+++ 05-usepopcorn/src/App.jsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
-import MoviesSearchList from './components/MoviesSearchList';
-import MoviesWatchList from './components/MoviesWatchList';
-// import { tempMovieData } from './temp_data';
+import WatchedSummary from './components/WatchedSummary';
+import WatchedMoviesList from './components/WatchedMoviesList';
+import MoviesList from './components/MoviesList';
 import Main from './components/Main';
+import Box from './components/Box';
 
 const KEY = '721c3a22';
 
 export default function App() {
   const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = 'hera pheri';
 
   useEffect(() => {
     async function fetchMovies() {
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+      setIsLoading(true);
+
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
       const data = await res.json();
       setMovies(data.Search);
+
+      setIsLoading(false);
     }
 
     fetchMovies();
@@ -26,8 +35,14 @@ export default function App() {
       <Navbar movies={movies} />
 
       <Main>
-        <MoviesSearchList movies={movies} />
-        <MoviesWatchList />
+        <Box>
+          <MoviesList movies={movies} />
+        </Box>
+
+        <Box>
+          <WatchedMoviesList watched={watched} />
+          <WatchedSummary watched={watched} />
+        </Box>
       </Main>
     </>
   );
