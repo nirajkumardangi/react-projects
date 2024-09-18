ad0a13479afb00a96848c554e2a741dc8dda4671
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-09-18 15:35:39 +0530

    handling sideEffects by using useEffect Hook

--- 05-usepopcorn/src/App.jsx
+++ 05-usepopcorn/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import MoviesSearchList from './components/MoviesSearchList';
 import MoviesWatchList from './components/MoviesWatchList';
-import { tempMovieData } from './temp_data';
+// import { tempMovieData } from './temp_data';
 import Main from './components/Main';
 
 const KEY = '721c3a22';
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
+  const query = 'hera pheri';
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+
+    fetchMovies();
+  }, []);
 
   return (
     <>
