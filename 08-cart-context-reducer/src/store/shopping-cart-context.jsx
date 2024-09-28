1353cfe36d2820687f46e0fe142d50051f3815cd
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-09-28 14:03:41 +0530

    creating and providing cart context

---
+++ 08-cart-context-reducer/src/store/shopping-cart-context.jsx
@@ -0,0 +1,80 @@
+import { createContext } from 'react';
+import { useState } from 'react';
+
+export const CartContext = createContext({
+  items: [],
+  handleAddItemToCart: () => {},
+  handleUpdateCartItemQuantity: () => {},
+});
+
+const crtValue = {
+  items: [],
+  addItemToCart: handleAddItemToCart,
+  updateItemQuantity: handleUpdateCartItemQuantity,
+};
+
+export default function CartContextProvider(children) {
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  return (
+    <CartContext.Provider value={crtValue}>{children}</CartContext.Provider>
+  );
+}
