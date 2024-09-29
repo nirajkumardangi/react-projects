9c096c709a1e58c9c7c49cef614ec1db68cf2a85
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-09-29 13:47:15 +0530

    delete modal after automatically after  3 seconds

--- 09-side-effect-useEffect/src/components/DeleteConfirmation.jsx
+++ 09-side-effect-useEffect/src/components/DeleteConfirmation.jsx
@@ -1,13 +1,27 @@
+import { useEffect, useRef } from 'react';
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const timerId = useRef();
+
+  useEffect(() => {
+    timerId.current = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timerId.current);
+    };
+  }, [onConfirm]);
+
   return (
-    <div id="delete-confirmation">
+    <div id='delete-confirmation'>
       <h2>Are you sure?</h2>
       <p>Do you really want to remove this place?</p>
-      <div id="confirmation-actions">
-        <button onClick={onCancel} className="button-text">
+      <div id='confirmation-actions'>
+        <button onClick={onCancel} className='button-text'>
           No
         </button>
-        <button onClick={onConfirm} className="button">
+        <button onClick={onConfirm} className='button'>
           Yes
         </button>
       </div>
