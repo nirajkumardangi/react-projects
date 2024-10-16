caa3b3459bf8ce0506f75ab64bf2d499e3a1740b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-16 10:16:47 +0530

    manage form data through state

--- 10-working-with-form/src/components/Login.jsx
+++ 10-working-with-form/src/components/Login.jsx
@@ -1,23 +1,45 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+
+  console.log(enteredEmail);
+  console.log(enteredPassword);
+
   return (
     <form>
       <h2>Login</h2>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+      <div className='control-row'>
+        <div className='control no-margin'>
+          <label htmlFor='email'>Email</label>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={enteredEmail}
+            onChange={(e) => setEnteredEmail(e.target.value)}
+          />
         </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+        <div className='control no-margin'>
+          <label htmlFor='password'>Password</label>
+          <input
+            id='password'
+            type='password'
+            name='password'
+            value={enteredPassword}
+            onChange={(e) => setEnteredPassword(e.target.value)}
+          />
         </div>
       </div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+      <p className='form-actions'>
+        <button className='button button-flat'>Reset</button>
+        <button className='button' onClick={handleSubmit}>
+          Login
+        </button>
       </p>
     </form>
   );
