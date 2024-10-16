6cebbeeda3225fdd516d7c44e5f4d6472323f3f9
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-16 15:36:44 +0530

    Delete 10-quiz-app directory

--- 10-quiz-app/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import quizLogo from '../assets/quiz-logo.png'
-export default function Header() {
-  return (
-    <header>
-      <img src={quizLogo} alt='Quiz Logo' />
-      <h1>React Quiz App</h1>
-    </header>
-  );
-}
