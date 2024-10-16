fbbbccecad1892207747700b39a2e3f9d69562f6
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-16 12:35:57 +0530

    validating input on every keystroke via state

--- 10-working-with-form/src/components/Login.jsx
+++ 10-working-with-form/src/components/Login.jsx
@@ -1,16 +1,25 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 export default function Login() {
-  const email = useRef();
-  const password = useRef();
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  const emailIsInvalid =
+    enteredValues.email !== '' && !enteredValues.email.includes('@');
 
-    const enteredEmail = email.current.value;
-    const enteredPassword = password.current.value;
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    console.log(enteredEmail, enteredPassword);
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
   }
 
   return (
@@ -20,22 +29,35 @@ export default function Login() {
       <div className='control-row'>
         <div className='control no-margin'>
           <label htmlFor='email'>Email</label>
-          <input id='email' type='email' name='email' ref={email} />
+          <input
+            id='email'
+            type='email'
+            name='email'
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
+          <div className='control-error'>
+            {emailIsInvalid && <p>please enter a valid email address!</p>}
+          </div>
         </div>
 
         <div className='control no-margin'>
           <label htmlFor='password'>Password</label>
-          <input id='password' type='password' name='password' ref={password} />
+          <input
+            id='password'
+            type='password'
+            name='password'
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
       <p className='form-actions'>
-        <button className='button button-flat' type='reset'>
-          Reset
-        </button>
-        <button className='button' type='submit'>
-          Login
-        </button>
+        <button className='button button-flat'>Reset</button>
+        <button className='button'>Login</button>
       </p>
     </form>
   );
