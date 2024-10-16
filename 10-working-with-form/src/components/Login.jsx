0bd55ee9ad03e29f9a57fd17f91caff13f27b254
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-16 13:01:51 +0530

    validate input upon lost focus

--- 10-working-with-form/src/components/Login.jsx
+++ 10-working-with-form/src/components/Login.jsx
@@ -6,8 +6,12 @@ export default function Login() {
     password: '',
   });
 
-  const emailIsInvalid =
-    enteredValues.email !== '' && !enteredValues.email.includes('@');
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -20,6 +24,11 @@ export default function Login() {
       ...prevValues,
       [identifier]: value,
     }));
+    setDidEdit((prevEdit) => ({ ...prevEdit, [identifier]: false }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({ ...prevEdit, [identifier]: true }));
   }
 
   return (
@@ -33,6 +42,7 @@ export default function Login() {
             id='email'
             type='email'
             name='email'
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
@@ -47,6 +57,7 @@ export default function Login() {
             id='password'
             type='password'
             name='password'
+            onBlur={() => handleInputBlur('password')}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
