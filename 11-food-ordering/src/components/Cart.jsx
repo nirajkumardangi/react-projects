0356c2000126efc14bf61bbda2ab06bc3b2af6bd
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-17 17:42:22 +0530

    working on the cart item

--- 11-food-ordering/src/components/Cart.jsx
+++ 11-food-ordering/src/components/Cart.jsx
@@ -4,6 +4,7 @@ import Button from '../UI/Button';
 import CartContext from '../store/CartContext';
 import { currencyFormatter } from '../utils/formatter.js';
 import UserProgressContext from '../store/UserProgressContext.jsx';
+import CartItem from './CartItem.jsx';
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -23,9 +24,12 @@ export default function Cart() {
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+            {...item}
+          />
         ))}
       </ul>
       <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
