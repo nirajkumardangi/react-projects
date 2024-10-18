c0d081bc81f35c3488de5804a2881b29987b1085
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-18 12:09:21 +0530

    handling HTTP loading & error states

--- 11-food-ordering/src/components/Meals.jsx
+++ 11-food-ordering/src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import MealItem from './MealItem';
 import useHttp from '../hooks/useHttp';
+import Error from './Error';
 
 const requestConfig = {};
 
@@ -11,7 +12,11 @@ export default function Meals() {
   );
 
   if (isLoading) {
-    return <p>Fetching meals...</p>;
+    return <p className='center'>Fetching meals...</p>;
+  }
+
+  if (error) {
+    return <Error title='Failed to fetch meals' message={error} />;
   }
 
   return (
