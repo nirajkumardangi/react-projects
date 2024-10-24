07f687788d5b07c1a99822ef547586c20278130c
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-24 18:23:50 +0530

    defining 2nd routes

--- 13-react-router/src/App.js
+++ 13-react-router/src/App.js
@@ -1,7 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
+import Products from './pages/Products';
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  {
+    path: '/products',
+    element: <Products />,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
