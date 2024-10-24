c830351ae0802882f772a702c354630cb73792fb
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-24 18:54:11 +0530

    Layouts & Nested Routes

--- 13-react-router/src/App.js
+++ 13-react-router/src/App.js
@@ -1,12 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import Root from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
   {
-    path: '/products',
-    element: <Products />,
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: '/', element: <Home /> },
+      { 
+        path: '/products',
+        element: <Products />,
+      },
+    ],
   },
 ]);
 
