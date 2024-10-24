b2ce16c184767b8749b2d153241e8660f2b441aa
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-24 20:54:32 +0530

    Understanding Relative and Absolute Path

--- 13-react-router/src/App.js
+++ 13-react-router/src/App.js
@@ -11,9 +11,9 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <Error />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/products', element: <Products /> },
-      { path: '/products/:productId', element: <ProductDetails /> },
+      { path: '', element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'products/:productId', element: <ProductDetails /> },
     ],
   },
 ]);
--- 13-react-router/src/pages/ProductDetails.js
+++ 13-react-router/src/pages/ProductDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 export default function ProductDetails() {
   const { productId } = useParams();
@@ -7,6 +7,9 @@ export default function ProductDetails() {
     <div>
       <b>ProductDetails</b>
       <p>Products ID: {productId}</p>
+      <p>
+        <Link to='..' relative='path'>Back</Link>
+      </p>
     </div>
   );
 }
