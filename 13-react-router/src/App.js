d89263ac8acb6e17429b12bfd2fe61512397bfe4
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-24 19:04:15 +0530

    Showing Error Pages with errorElement

--- 13-react-router/src/App.js
+++ 13-react-router/src/App.js
@@ -2,14 +2,16 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Root from './pages/Root';
+import Error from './pages/Error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
-      { 
+      {
         path: '/products',
         element: <Products />,
       },
