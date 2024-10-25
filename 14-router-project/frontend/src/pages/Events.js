50581e4173925db2dc9eafe838992526b49668dc
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-25 17:51:18 +0530

    Extracting Error Data & Throwing Responses

--- 14-router-project/frontend/src/pages/Events.js
+++ 14-router-project/frontend/src/pages/Events.js
@@ -24,7 +24,10 @@ export async function loader() {
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.' };
-    throw { message: 'Could not fetch events.' };
+    // throw { message: 'Could not fetch events.' };
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    });
   } else {
     return response;
   }
