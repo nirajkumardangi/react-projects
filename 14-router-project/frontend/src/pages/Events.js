0a43665901f8991203bb7f8795e28be86b847f1a
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-25 18:02:32 +0530

    The json() Utility Function

--- 14-router-project/frontend/src/pages/Events.js
+++ 14-router-project/frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
+import { json, useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
@@ -20,14 +20,15 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch('http://localhost:8080/events');
+  const response = await fetch('http://localhost:8080/eventsjj');
 
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events.' };
     // throw { message: 'Could not fetch events.' };
-    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
     return response;
   }
