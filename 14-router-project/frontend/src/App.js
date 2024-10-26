bc29dc540674e84d88ca6441695fec93f83a5131
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-26 19:56:25 +0530

    Reusing Actions via Request Methods

--- 14-router-project/frontend/src/App.js
+++ 14-router-project/frontend/src/App.js
@@ -7,10 +7,11 @@ import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from './pages/EventDetail';
-import NewEventPage, { action as newEventAction } from './pages/NewEvent';
+import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
+import { action as manipulateEventAction } from './components/EventForm';
 
 const router = createBrowserRouter([
   {
@@ -41,10 +42,15 @@ const router = createBrowserRouter([
               {
                 path: 'edit',
                 element: <EditEventPage />,
+                action: manipulateEventAction,
               },
             ],
           },
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
       },
     ],
--- 14-router-project/frontend/src/pages/EditEvent.js
+++ 14-router-project/frontend/src/pages/EditEvent.js
@@ -4,5 +4,5 @@ import EventForm from '../components/EventForm';
 export default function EditEventPage() {
   const data = useRouteLoaderData('event-detail');
 
-  return <EventForm event={data.event} />;
+  return <EventForm event={data.event} method='patch'/>;
 }
