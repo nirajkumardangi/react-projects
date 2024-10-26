4b6c4be064a5dee29611a4ffdb997c3239108bb0
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-10-26 17:29:33 +0530

    Working with action() Functions

--- 14-router-project/frontend/src/App.js
+++ 14-router-project/frontend/src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/Home';
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
+import NewEventPage, { action as newEventAction } from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
               { path: 'edit', element: <EditEventPage /> },
             ],
           },
-          { path: 'new', element: <NewEventPage /> },
+          { path: 'new', element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
